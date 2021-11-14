964aa5960caacd9168c3840e01db61def08d34d0
zjxte <ip114@msn.com>
2021-11-14 14:29:37 +0000

    Add project files.

---
+++ ExcelAddInDataSet/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Data.OleDb;
+
+namespace ExcelAddInDataSet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string myDB = "ProductNotes-Search.xlsx";
+                System.Data.OleDb.OleDbConnection myConnection;
+                DataSet dtset;
+                System.Data.OleDb.OleDbDataAdapter myCommand;
+                myConnection = new System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + myDB + ";Extended Properties=dBASE IV;");
+                myCommand = new System.Data.OleDb.OleDbDataAdapter("SELECT * FROM [Sheet1$}", myConnection);
+                myCommand.TableMappings.Add("Table", "TestTable");
+                dtset = new System.Data.DataSet();
+                myCommand.Fill(dtset);
+                dataGridView1.DataSource = dtset.Tables[0];
+                myConnection.Close();
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ ExcelAddInDataSet/Ribbon1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+
+namespace ExcelAddInDataSet
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+//show form1
+
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            //get notes for column G
+            Worksheet currentSheet = Globals.ThisAddIn.GetActiveWorkSheet();
+            var activeApp = Globals.ThisAddIn.GetActiveApp();
+
+            int sRowCount = currentSheet.UsedRange.Rows.Count;
+            Range sNum = currentSheet.Range["A2:A" + sRowCount];
+
+
+        }
+    }
+}
